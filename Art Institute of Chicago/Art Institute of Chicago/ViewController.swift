3b41fffc510d1051f683c38243a1d200def5dbf5
Korotkova Daria <Dkuvashova@mail.ru>
2023-11-22 11:56:01 +0300

    Add project
    Add project and do launchScreen

---
+++ Art Institute of Chicago/Art Institute of Chicago/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Art Institute of Chicago
+//
+//  Created by Daria on 22.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+
+}
+
